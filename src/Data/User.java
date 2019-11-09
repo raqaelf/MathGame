4763201a66f2db6f3c060a4a8f001594ff18d761
dhaniaffa <dhaniaffa@github.com>
2019-11-09 23:58:46 +0700

    Create User.java
    Data untuk User

---
+++ src/Data/User.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Data;
+
+/**
+ *
+ * @author asus
+ */
+public class User {
+    public String nama;
+    private int lives;
+    private int score;
+
+    public User() {
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getLives() {
+        return lives;
+    }
+
+    public void setLives(int lives) {
+        this.lives = lives;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
